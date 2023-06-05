15003bbefbf6a481efada2e2840826f001f474ea
MrTatsugoro <mrtatsugoro@gmail.com>
2023-06-05 23:01:18 +0200

    Fix timing.py
    The previous implementation ensured side-effect-free execution by having
    the input list deepcopied as part of the benched function. This affects
    timings too much, so I moved that to the setup and used
    timeit.repeat(repeat=x, number=1) instead of timeit.timeit(number=x) to
    ensure the setup runs before each iteration. No side-effects and we
    bench the actual code instead of the deepcopy. I also simplified some of
    the code to remove redundant stuff. + some PEP8.

--- timing.py
+++ timing.py
@@ -1,9 +1,12 @@
-import core
-import timeit
-import random
-import sys
 import argparse
+import core
+import json
+from pathlib import Path
+import random
+from statistics import mean
+import sys
 import time
+import timeit
 
 
 # =================
@@ -11,16 +14,12 @@ import time
 # =================
 
 class Benchmark:
-
     def __init__(self):
-        with open('core.py') as f:
-            self.setup = f.read()
-
-        self.setup += """\nfrom copy import deepcopy\n"""
-
+        self.lsts = []
+        self.info = ''
+        self.setup = 'from copy import deepcopy\nlists = deepcopy(lsts)'
         self.load()
         self.build_info()
-        self.extend_setup()
 
     def load(self):
         raise NotImplementedError
@@ -34,107 +33,109 @@ class Benchmark:
             if len(lst) > max:
                 max = len(lst)
             num += 1
-        self.info = "{} lists, average size {}, max size {}".format(num, size//num, max)
-
-    def extend_setup(self):
-        self.setup += ''
-        raise NotImplementedError
+        self.info = f'{num} lists, average size {size // num}, max size {max}'
 
 
 class Niklas(Benchmark):
-
     def __init__(self, filename):
         self.filename = filename
         super().__init__()
 
     def load(self):
-        self.lsts = []
-        with open(self.filename, "r") as f:
-            for line in f:
-                lst = [int(x) for x in line.split()]
-                self.lsts.append(lst)
+        self.lsts = [
+            [int(x) for x in line.split()]
+            for line in Path(self.filename).read_text().splitlines()
+        ]
 
     def build_info(self):
         super().build_info()
-        self.info += '\n(from file: {})'.format(self.filename)
-
-    def extend_setup(self):
-        self.setup += """
-
-lsts = []
-size = 0
-num = 0
-max = 0
-for line in open("{0}", "r"):
-  lst = [int(x) for x in line.split()]
-  size += len(lst)
-  if len(lst) > max: max = len(lst)
-  num += 1
-  lsts.append(lst)
-""".format(self.filename)
+        self.info += f'\n(from file: {self.filename})'
 
 
 class Sven(Benchmark):
-
     def load(self):
-        import json
-        with open('./lists/sven_list.txt') as f:
-            self.lsts = json.loads(f.read())
-
-    def extend_setup(self):
-        self.setup += """
-import json
-with open('./lists/sven_list.txt') as f:
-    lsts = json.loads(f.read())
-"""
+        self.lsts = json.loads(Path('./lists/sven_list.txt').read_text())
 
 
 class Agf(Benchmark):
-
     def load(self):
-        import random
-        tenk = range(10000)
-        self.lsts = [random.sample(tenk, random.randint(0, 500)) for _ in range(2000)]
-
-    def extend_setup(self):
-        self.setup += """\nlsts = {}""".format(repr(self.lsts))
+        self.lsts = [
+            random.sample(range(10000), random.randint(0, 500))
+            for _ in range(2000)
+        ]
 
 
 # ======================================
 # Function for building Nik's test lists
 # ======================================
 
-def build_timing_list(filename,
-                      class_count=50,
-                      class_size=1000,  
-                      list_count_per_class=10,
-                      large_list_sizes = (100, 1000),
-                      small_list_sizes = (0, 100),
-                      large_list_probability = 0.5):
+def build_timing_list(
+    filename,
+    class_count=50,
+    class_size=1000,
+    list_count_per_class=10,
+    large_list_sizes=(100, 1000),
+    small_list_sizes=(0, 100),
+    large_list_probability=0.5
+):
+    large_list_sizes = list(range(*large_list_sizes))
+    small_list_sizes = list(range(*small_list_sizes))
+    with open(filename, "w") as f:
+        lists = []
+        classes = [
+            list(range(class_size * i, class_size * (i + 1)))
+            for i in range(class_count)
+        ]
+        for c in classes:
+            # distribute each class across ~300 lists
+            for i in range(list_count_per_class):
+                lst = []
+                if random.random() < large_list_probability:
+                    size = random.choice(large_list_sizes)
+                else:
+                    size = random.choice(small_list_sizes)
+                nums = set(c)
+                for j in range(size):
+                    x = random.choice(list(nums))
+                    lst.append(x)
+                    nums.remove(x)
+                random.shuffle(lst)
+                lists.append(lst)
+        random.shuffle(lists)
+        for lst in lists:
+            f.write(" ".join(str(x) for x in lst) + "\n")
 
-  large_list_sizes = list(range(*large_list_sizes))
-  small_list_sizes = list(range(*small_list_sizes))
-  with open(filename, "w") as f:
-    lists = []
-    classes = [list(range(class_size*i, class_size*(i+1))) for i in range(class_count)]
-    for c in classes:
-      # distribute each class across ~300 lists
-      for i in range(list_count_per_class):
-        lst = []
-        if random.random() < large_list_probability:
-          size = random.choice(large_list_sizes)
-        else:
-          size = random.choice(small_list_sizes)
-        nums = set(c)
-        for j in range(size):
-          x = random.choice(list(nums))
-          lst.append(x)
-          nums.remove(x)
-        random.shuffle(lst)
-        lists.append(lst)
-    random.shuffle(lists)
-    for lst in lists:
-      f.write(" ".join(str(x) for x in lst) + "\n")
+
+def build_all_timing_lists():
+    print('building test list (for Nik test) ... ', end='')
+    sys.stdout.flush()
+    param = dict(class_count=50,
+                 class_size=1000,
+                 list_count_per_class=100,
+                 large_list_sizes=(100, 1000),
+                 small_list_sizes=(0, 100),
+                 large_list_probability=0.5,
+                 filename='./lists/timing_1.txt')
+    build_timing_list(**param)
+
+    param = dict(class_count=15,
+                 class_size=1000,
+                 list_count_per_class=300,
+                 large_list_sizes=(100, 1000),
+                 small_list_sizes=(0, 100),
+                 large_list_probability=0.5,
+                 filename='./lists/timing_2.txt')
+    build_timing_list(**param)
+
+    param = dict(class_count=15,
+                 class_size=1000,
+                 list_count_per_class=300,
+                 large_list_sizes=(100, 1000),
+                 small_list_sizes=(0, 100),
+                 large_list_probability=0.1,
+                 filename='./lists/timing_3.txt')
+    build_timing_list(**param)
+    print('done')
 
 
 # ===============
@@ -144,8 +145,7 @@ def build_timing_list(filename,
 def timing(bench, number):
     print('\nTiming with: >> {} << Benchmark'.format(bench.__class__.__name__))
     print('Info: {}'.format(bench.info))
-    setup = bench.setup
-    
+
     print('-- Press Ctrl-C to skip a test --\n')
 
     times = []
@@ -154,9 +154,21 @@ def timing(bench, number):
             print('timing: {} '.format(value.__doc__), end='')
             sys.stdout.flush()
             try:
-                t = timeit.timeit("{}(deepcopy(lsts))".format(name),
-                                  setup=setup,
-                                  number=number)
+                # We pass number to repeat and leave number to 1.
+                # This ensures the setup is repeated before every
+                # iteration. We put `bench.lsts` into the execution
+                # namespace. The setup does the deepcopy into `lists`
+                # (and this deepcopy doesn't count in timings). The
+                # benched execution uses the deepcopied list. This way,
+                # if any function has side effects, they don't impact
+                # other runs.
+                t = mean(timeit.repeat(
+                    f'{name}(lists)',
+                    setup=bench.setup,
+                    number=1,
+                    repeat=number,
+                    globals={'lsts': bench.lsts, **core.__dict__}
+                ))
             except KeyboardInterrupt:
                 print(' skipped.')
                 try:
@@ -164,14 +176,17 @@ def timing(bench, number):
                 except KeyboardInterrupt:
                     print('Two fast Ctrl-C - exiting')
                     sys.exit(0)
-                
+
             else:
                 times.append((t, value.__doc__))
                 print(' --   {:0.4f}   -- '.format(t))
 
     print('\nTiming Results:')
-    for t,name in sorted(times):
-        print('{:0.3f}  -- {}'.format(t,name))
+    times = sorted(times)
+    best_t, best_name = times[0]
+    for t, name in times:
+        factor = t / best_t
+        print(f'{t:0.3f} ({factor:.2g}x) -- {name}')
 
 
 if __name__ == '__main__':
@@ -182,36 +197,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.new:
-        print('building test list (for Nik test) ... ', end='')
-        sys.stdout.flush()
-        param = dict(class_count = 50,
-                     class_size = 1000,
-                     list_count_per_class = 100,
-                     large_list_sizes = (100, 1000),
-                     small_list_sizes = (0, 100),
-                     large_list_probability = 0.5,
-                     filename = './lists/timing_1.txt')
-        build_timing_list(**param)
+        build_all_timing_lists()
 
-        param = dict(class_count = 15,
-                     class_size = 1000,
-                     list_count_per_class = 300,
-                     large_list_sizes = (100, 1000),
-                     small_list_sizes = (0, 100),
-                     large_list_probability = 0.5,
-                     filename = './lists/timing_2.txt')
-        build_timing_list(**param)
-        
-        param = dict(class_count = 15,
-                     class_size = 1000,
-                     list_count_per_class = 300,
-                     large_list_sizes = (100, 1000),
-                     small_list_sizes = (0, 100),
-                     large_list_probability = 0.1,
-                     filename = './lists/timing_3.txt')
-        build_timing_list(**param)
-        print('done')
-        
     timing(Niklas('./lists/timing_1.txt'), number=3)
     timing(Niklas('./lists/timing_2.txt'), number=3)
     timing(Niklas('./lists/timing_3.txt'), number=3)
